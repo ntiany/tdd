3b9ffb207279704a3671dc025751b14b1856ee8e
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-09-06 14:19:01 +0200

    Fix tests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Tdd;
 using Xunit;
 
@@ -11,49 +13,107 @@ namespace Tests
         [Fact]
         public void ShouldReturnFizz()
         {
-            var generatedNumber = ReturnDivisableNumber(3);
+            // should add option to return 0
+            var baseNumber = ReturnDivisibleNumber(15);
+            var divisbleBy3List = GenerateListWithDivisibleNumbers(3, 15);
+            var testNumber = baseNumber + divisbleBy3List[ReturnRandomNumber(divisbleBy3List.Count)];
 
-            var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
+            var fizzBuzzResult = FizzBuzzHandler.Handle(testNumber);
 
-            Assert.Equal(fizzBuzzResult, "Fizz");
+            Assert.Equal("Fizz", fizzBuzzResult);
         }
 
         [Fact]
         public void ShouldReturnBuzz()
         {
-            var generatedNumber = ReturnDivisableNumber(5);
+            // should add option to return 0
+            var baseNumber = ReturnDivisibleNumber(15);
+            var divisbleBy5List = GenerateListWithDivisibleNumbers(5, 15);
+            var testNumber = baseNumber + divisbleBy5List[ReturnRandomNumber(divisbleBy5List.Count)];
 
-            var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
+            var fizzBuzzResult = FizzBuzzHandler.Handle(testNumber);
 
-            Assert.Equal(fizzBuzzResult, "Buzz");
+            Assert.Equal("Buzz", fizzBuzzResult);
         }
 
         [Fact]
         public void ShouldReturnFizzBuzz()
         {
-            var generatedNumber = ReturnDivisableNumber(15);
+            
+            var generatedNumber = ReturnDivisibleNumber(15);
 
             var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
 
-            Assert.Equal(fizzBuzzResult, "FizzBuzz");
+            Assert.Equal("FizzBuzz", fizzBuzzResult);
         }
 
         [Fact]
         public void ShouldReturnNumber()
         {
-            var generatedNumber = ReturnDivisableNumber(15);
-            var testNumber = generatedNumber + 1;
+            var baseNumber = ReturnDivisibleNumber(15);
+
+            var notDivisibleBy3 = GenerateListWithNotDivisibleNumbers(3, 15);
+            var notDivisibleBy5 = GenerateListWithNotDivisibleNumbers(5, 15);
+            var allowedNumbersList = notDivisibleBy3.Intersect(notDivisibleBy5).ToList();
+
+            var testNumber = baseNumber + allowedNumbersList[ReturnRandomNumber(allowedNumbersList.Count)];
             var fizzBuzzResult = FizzBuzzHandler.Handle(testNumber);
 
-            Assert.Equal(fizzBuzzResult, testNumber.ToString());
+            Assert.Equal(testNumber.ToString(), fizzBuzzResult);
 
         }
 
-        private int ReturnDivisableNumber(int number)
+        private int ReturnRandomNumber(int upperBound, int lowerBound = 0)
         {
             var random = new Random();
-            var generatedNumber = random.Next(boundary / number) * number;
+            if (lowerBound == 0)
+            {
+                return random.Next(upperBound);
+            }
+            else
+            {
+                return random.Next(lowerBound, upperBound);
+            }
+
+        }
+
+        private int ReturnDivisibleNumber(int number)
+        {
+            var lowerBound = 1;
+            var generatedNumber = ReturnRandomNumber(boundary / number, lowerBound) * number;
             return generatedNumber;
         }
+
+        private List<int> GenerateListWithDivisibleNumbers(int divisionNumber, int maxNumber)
+        {
+            List<int> newList = new List<int>();
+            for (int i = 1; i < maxNumber; i++)
+            {
+                if (i % divisionNumber == 0)
+                {
+                    newList.Add(i);
+                }
+            }
+
+            return newList;
+        }
+
+
+
+        private List<int> GenerateListWithNotDivisibleNumbers(int divisionNumber, int maxNumber)
+        {
+            List<int> newList = new List<int>();
+            for (int i = 1; i < maxNumber; i++)
+            {
+                if (i % divisionNumber != 0)
+                {
+                    newList.Add(i);
+                }
+            }
+
+            return newList;
+        }
+
+
     }
 }
