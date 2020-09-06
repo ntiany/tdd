594e9d4b5dbe47b81c337f23f5cbb6afa4116481
Aneta-Dudek <57062491+Aneta-Dudek@users.noreply.github.com>
2020-09-06 13:22:21 +0200

    Develop further

--- Tdd/FizzBuzzHandler.cs
+++ Tdd/FizzBuzzHandler.cs
@@ -8,7 +8,22 @@ namespace Tdd
     {
         public static string Handle(int number)
         {
-            return null;
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
         }
     }
 }
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -6,30 +6,54 @@ namespace Tests
 {
     public class Tests
     {
+        private const int boundary = 100;
+
         [Fact]
         public void ShouldReturnFizz()
         {
-            var random = new Random();
-            var number = random.Next(33) * 3;
-            var generatedNumber = FizzBuzzHandler.Handle(number);
+            var generatedNumber = ReturnDivisableNumber(3);
+
+            var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
+
+            Assert.Equal(fizzBuzzResult, "Fizz");
         }
 
         [Fact]
         public void ShouldReturnBuzz()
         {
+            var generatedNumber = ReturnDivisableNumber(5);
 
+            var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
+
+            Assert.Equal(fizzBuzzResult, "Buzz");
         }
 
         [Fact]
         public void ShouldReturnFizzBuzz()
         {
+            var generatedNumber = ReturnDivisableNumber(15);
 
+            var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
+
+            Assert.Equal(fizzBuzzResult, "FizzBuzz");
         }
 
         [Fact]
         public void ShouldReturnNumber()
         {
+            var generatedNumber = ReturnDivisableNumber(15);
+            var testNumber = generatedNumber + 1;
+            var fizzBuzzResult = FizzBuzzHandler.Handle(generatedNumber);
 
+            Assert.Equal(fizzBuzzResult, testNumber.ToString());
+
+        }
+
+        private int ReturnDivisableNumber(int number)
+        {
+            var random = new Random();
+            var generatedNumber = random.Next(boundary / number) * number;
+            return generatedNumber;
         }
     }
 }
